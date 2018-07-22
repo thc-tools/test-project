9311194b994987d91acbeb47ed83fc21b81db376
Hartorn <bazirehoussin@gmail.com>
2018-07-22 23:39:29 +0200

    feat: Updating to latest version

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { hot } from 'react-hot-loader'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { hot } from "react-hot-loader";
 // import { Button } from 'rmwc/Button';
 // import '@material/button/dist/mdc.button.css';
-import './app.css';
+import "./app.css";
 
 const test = {
     truc: {
-        bidule: 'lol'
+        bidule: "lol"
     }
-}
+};
 
 class App extends Component {
     render() {
         return (
             <div>
-                {'Test app'}
+                {"Test app ohohoh"}
                 <div>{test?.truc?.bidule}</div>
-                <div>{test?.lol?.bidule === undefined && 'Undefined' }</div>
-                <div>{test?.lol?.bidule ?? 'hihi'}</div>
-                <div className='icon icon-test'/>
+                <div>{test?.lol?.bidule === undefined && "Undefined"}</div>
+                <div>{test?.lol?.bidule ?? "hihi"}</div>
+                <div className="icon icon-test" />
                 {/* <Button >Test Button</Button> */}
             </div>
         );
     }
 }
 
-App.propTypes = {
+App.propTypes = {};
 
-};
-
-export default hot(module)(App)
+export default hot(module)(App);
