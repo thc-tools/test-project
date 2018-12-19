db2cfa7c0ae289fb0c87e21f5c399ed7192a0366
Hartorn <bazirehoussin@gmail.com>
2018-12-19 01:29:34 +0100

    Removing old conf

--- dev-server.js
+++
@@ -1,6 +0,0 @@
-const devServer = require('@thc/webpack-react/lib/dev-server');
-const webpackConf = require('./webpack.config.js');
-
-const customConfig = {};
-
-devServer(webpackConf(process.env), customConfig);
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const configFunc = require('@thc/webpack-react/lib/postcss-config');
-
-module.exports = ({ file, options, env }) => {
-    const config = configFunc({ file, options, env });
-    // Do what you want here
-    return config;
-};
